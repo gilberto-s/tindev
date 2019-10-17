1326dd05623074e8a51794780c6afd37b8edc62a
Gilberto Silva <gsds23@gmail.com>
2019-10-16 23:20:18 -0300

    Configuring match on web app

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,34 @@ const cors = require('cors');
 
 const routes = require("./routes");
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-mongoose.connect(
-    'mongodb+srv://omnistack:omnistack@cluster0-ebdd2.mongodb.net/test?retryWrites=true&w=majority'
-    //'mongodb://localhost:27017/omnistack8'
-    , {
-    useNewUrlParser: true
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    console.log(user, socket.id);
+
+    connectedUsers[user] = socket.id;
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+mongoose.connect(
+    'mongodb+srv://omnistack:omnistack@cluster0-ebdd2.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
